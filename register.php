a0610d11a981638527f7124d040f4b2ecf62fd73
kriptografi-202501-230202793 <164715142+Zalsabilahna@users.noreply.github.com>
2026-01-29 14:30:57 +0700

    Update register.php

--- register.php
+++ register.php
@@ -13,8 +13,8 @@ if (isset($_SESSION['username'])) {
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = hash('sha256', $_POST['password']); // Hash the input password using SHA-256
-    $cpassword = hash('sha256', $_POST['cpassword']); // Hash the input confirm password using SHA-256
+    $password = hash('sha256', $_POST['password']);
+    $cpassword = hash('sha256', $_POST['cpassword']);
  
     if ($password == $cpassword) {
         $sql = "SELECT * FROM users WHERE email='$email'";
@@ -33,7 +33,7 @@ if (isset($_POST['submit'])) {
                 echo "<script>alert('Maaf, terjadi kesalahan.')</script>";
             }
         } else {
-            echo "<script>alert('Ups, email Sudah Terdaftar.')</script>";
+            echo "<script>alert('Ups, email sudah terdaftar.')</script>";
         }
     } else {
         echo "<script>alert('Password tidak sesuai.')</script>";
@@ -53,28 +53,38 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Daftar Akun Hostinger</title>
+    <title>Daftar | Laundry Clean & Fresh</title>
 </head>
 <body>
     <div class="container">
         <form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Gabung Hostinger</p>
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">
+                Daftar Laundry
+            </p>
+            <p style="text-align:center; margin-bottom: 15px;">
+                Bergabung dengan <b>Laundry Clean & Fresh</b>
+            </p>
+
             <div class="input-group">
-                <input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+                <input type="text" placeholder="Nama Pengguna" name="username" value="<?php echo $username; ?>" required>
             </div>
             <div class="input-group">
-                <input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+                <input type="email" placeholder="Email Pelanggan" name="email" value="<?php echo $email; ?>" required>
             </div>
             <div class="input-group">
                 <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+                <input type="password" placeholder="Konfirmasi Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
             </div>
             <div class="input-group">
                 <button name="submit" class="btn">Daftar</button>
             </div>
-            <p class="login-register-text">Sudah punya akun? <a href="index.php">Login</a>.</p>
+
+            <p class="login-register-text">
+                Sudah punya akun laundry? 
+                <a href="index.php">Login di sini</a>
+            </p>
         </form>
     </div>
 </body>
