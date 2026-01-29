ff259fbd9257e31c391308a51458655c2be736cd
kriptografi-202501-230202793 <164715142+Zalsabilahna@users.noreply.github.com>
2026-01-29 14:26:33 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 $server = "192.168.169.9";
-$user = "root"
+$user = "root";
 $pass = "12345678";
 $database = "login";
 $conn = mysqli_connect($server, $user, $pass, $database);
 if (!$conn) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
-
 ?>
+
 
