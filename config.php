306b66bc3583caccb370246703cdc1ab127b5b68
kriptografi-202501-230202793 <164715142+Zalsabilahna@users.noreply.github.com>
2026-01-29 14:24:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $server = "192.168.169.9";
-$user = "root
+$user = "root"
 $pass = "12345678";
 $database = "login";
 $conn = mysqli_connect($server, $user, $pass, $database);
@@ -9,3 +9,4 @@ if (!$conn) {
 }
 
 ?>
+
