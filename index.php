83f525fc00da9d90d86b131181a8055813ec9965
kriptografi-202501-230202793 <164715142+Zalsabilahna@users.noreply.github.com>
2026-01-29 14:21:36 +0700

    mumet

--- index.php
+++ index.php
@@ -35,22 +35,35 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>PHP Login Page</title>
+    <title>Login | Laundry Clean & Fresh</title>
 </head>
 <body>
     <div class="container">
         <form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">
+                Login Laundry
+            </p>
+
+            <p style="text-align:center; margin-bottom:15px;">
+                Selamat datang di <b>Laundry Clean & Fresh</b>
+            </p>
+
             <div class="input-group">
-                <input type="email" placeholder="Email" name="email" required>
+                <input type="email" placeholder="Email Pelanggan" name="email" required>
             </div>
+
             <div class="input-group">
                 <input type="password" placeholder="Password" name="password" required>
             </div>
+
             <div class="input-group">
-                <button name="submit" class="btn">Login</button>
+                <button name="submit" class="btn">Masuk</button>
             </div>
-            <p class="login-register-text">Belum punya akun? <a href="register.php">Daftar</a>.</p>
+
+            <p class="login-register-text">
+                Belum punya akun laundry?
+                <a href="register.php">Daftar Sekarang</a>
+            </p>
         </form>
     </div>
 </body>
